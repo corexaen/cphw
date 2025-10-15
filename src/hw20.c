2995255367388d3eb3739e0f4757ea72147ce855
corexaen <nannom06@naver.com>
2025-10-15 15:39:15 +0900

    hw20(2)

--- src/hw20.c
+++ src/hw20.c
@@ -2,10 +2,10 @@
 char _404(char* str) {
     unsigned int arr = 0;
     for(int i = 0; str[i] != '\0'; i++) {
-        arr |= (1 << (str[i] - 'a'));
+        arr |= (1 << (str[i] - 'a'));           // 등장한 문자는 1
     }
     for(int i = 0; i < 26; i++) {
-        if(!(arr & (1 << i))) return 'a' + i;
+        if(!(arr & (1 << i))) return 'a' + i;   // 등장하지 않은 문자 찾기
     }
     return 0;
 }
