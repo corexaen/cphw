781b2c9d1d77f803929191491598c7e7fd66fd39
corexaen <nannom06@naver.com>
2025-10-15 11:07:42 +0900

    hw10

---
+++ src/hw10.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+typedef struct __Time {
+    int hour;
+    int minute;
+} Time;
+Time add_time(const Time t1, const Time t2) {
+    Time result;
+    result.hour = (t1.hour + t2.hour + (t1.minute + t2.minute) / 60) % 24;
+    result.minute = (t1.minute + t2.minute) % 60;
+    return result;
+}
+int main() {
+    Time t1 = {21, 0}, t2 = {0, 0};
+    scanf("%d", &t2.minute);
+
+    Time t3 = add_time(t1, t2);
+    printf("%02d:%02d\n", t3.hour, t3.minute);
+    return 0;
+}
