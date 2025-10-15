372f27a5e444bd9161c7f89c066762811c2a081f
corexaen <nannom06@naver.com>
2025-10-15 13:28:29 +0900

    hw15

---
+++ src/hw15.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+const char* translate(const char* str) {
+    const char* know_word[3] = {"red", "blue", "green"};
+    const char* new_word[3] = {"SSS", "FFF", "MMM"};
+    for (int i = 0; i < 3; i++) {
+        if (strcmp(str, know_word[i]) == 0) {
+            return new_word[i];
+        }
+    }
+    return "Unknown";
+}
+
+int main() {
+    char input[11] = {0,};
+    scanf("%10s", input);
+    const char* output = translate(input);
+    printf("%s\n", output);
+    return 0;
+}
