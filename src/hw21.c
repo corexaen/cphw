5f425f42bd5e3f9ded1eb13bf52550fda77dd56b
corexaen <nannom06@naver.com>
2025-10-15 18:39:54 +0900

    hw21(2)

--- src/hw21.c
+++ src/hw21.c
@@ -4,8 +4,8 @@
 
 void CL(char* str, char* buff) {
     while(*str != '\0') {
-        if(*str >= 'A' && *str <= 'Z') {
-            *(buff++) = *str;
+        if(*str >= 'A' && *str <= 'Z') {    // 대문자이면
+            *(buff++) = *str;               // 저장
         }
         str++;
     }
@@ -17,7 +17,7 @@ int main() {
     char* buff = (char*)malloc(strlen(str) + 1);
     memset(buff, 0, strlen(str) + 1);
     CL(str, buff);
-    if(*buff == '\0')
+    if(*buff == '\0')               // 저장된 문자가 없으면
         printf("Nothing\n");
     else
         printf("%s\n", buff);
